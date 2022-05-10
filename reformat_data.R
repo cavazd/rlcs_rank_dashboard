3fb15e64f89763ad57c41c049fe01182858756b7
cavazd <59621004+cavazd@users.noreply.github.com>
2022-05-10 13:50:03 -0700

    Add reformat_data.R and use it to create rlcs_na_ranks.csv and rlcs_eu_ranks.csv

---
+++ reformat_data.R
@@ -0,0 +1,57 @@
+# -------------------------
+# author: cavazd
+# date  : 5/10/2022
+# desc  : Code to restructure rlcs_[region]_points.csv for bump chart
+# -------------------------
+
+# Load in tidyverse and remove and variables in environment
+library(tidyverse)
+rm(list=ls())
+
+# load in data for the given region
+# -------------------------
+
+region <- "eu"
+
+score_df <- read.csv(
+  file = paste0("data/rlcs_", region, "_points.csv"),
+  stringsAsFactors = T,
+)
+
+# create empty dataframe for dataframe stacking
+# -------------------------
+num_events <- ncol(score_df[-1]) # num of cols without team_name
+
+rank_df <- data.frame()
+
+# add rankings for each event to  rank_df
+# -------------------------
+for (event in 1:num_events) {
+  # save to a temp dataframe, select variables from start upto current event
+  temp_df <- score_df %>% select(1:(event+1)) %>%
+    # add the score by taking the sum of all event vars
+    # add event as an int of the current event
+    mutate('score' = rowSums(select(., 2:(event+1))),
+           'event' = event) %>%
+    # remove the different event vars
+    select(-(2:(event+1))) %>%
+    # arrange by score and team (alphabetical)
+    arrange(desc(score), team_name) %>%
+    # add ranking after sorting (1 to number of teams)
+    # add the event name in as well
+    mutate('ranking' = c(1:nrow(.)),
+           'eventname' = colnames(score_df)[event+1])
+
+  # append the temporary dataframe onto rank_df
+  rank_df <- rbind(rank_df, temp_df)
+}
+
+# write transformed data to a new csv file
+# -------------------------
+write.csv(
+  x = rank_df,
+  file = paste0("data/rlcs_", region, "_ranks.csv"),
+  row.names = F
+)
+
+
